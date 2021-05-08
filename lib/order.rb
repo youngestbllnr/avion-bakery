53820562172ae4cc80e29feb16dc0f337d3d7fbe
Carl Rosales <rosales.carlangel@gmail.com>
2021-05-08 22:31:07 +0800

    Created order.rb

---
+++ lib/order.rb
@@ -0,0 +1,25 @@
+require_relative './product'
+require_relative './services/packer'
+
+class Order
+  attr_reader :breakdown, :cost
+
+  def initialize(product_code, quantity)
+    @product_code = product_code
+    @quantity = quantity
+    @breakdown = []
+    @cost = 0.00
+  end
+
+  def process
+    # Call the packer service
+    @breakdown, @cost = Service::Packer.call(@product_code, @quantity)
+
+    # Format the output
+    output = "#{@quantity} #{@product_code} $#{@cost}"
+    @breakdown.each { |pack| output += "\n\t#{pack}" }
+
+    # Return the output
+    [output, @breakdown, @cost]
+  end
+end
